37e68491a49afbf10b6b094b91619e4caea5f620
kissateig <70164911+kissateig@users.noreply.github.com>
2020-08-27 17:16:05 +0200

    Update leibniz_way_of_finding_pi.py

--- Pi/leibniz_way_of_finding_pi.py
+++ Pi/leibniz_way_of_finding_pi.py
@@ -1,3 +1,6 @@
+# This is also the taylor expansion of arctan(x) for x = 1, where we multiplied both sides by 4,
+# we then get pi = 4 - 4/3 + 4/5 - 4/7 + 4/9..., the right side of this equation is given in the formula below
+
 def pi():
     n = 1
     current, later = 4, 4
