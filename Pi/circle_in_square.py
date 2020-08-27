8006a4fdd03d0352a20674d32d31b7f972c1f7e0
kissateig <70164911+kissateig@users.noreply.github.com>
2020-08-27 17:05:06 +0200

    Update circle_in_square.py

--- Pi/circle_in_square.py
+++ Pi/circle_in_square.py
@@ -2,19 +2,20 @@
 
 import numpy as np
 
+# Calculate the distance from origin in the first quadrant
 def coord():
-    distance  = np.random.rand() ** 2 + np.random.rand() ** 2
+    distance  = np.sqrt(np.random.rand() ** 2 + np.random.rand() ** 2)
     return distance
 
 outside = 0
 inside = 0
-at = 0
 
+# Plot numbers in or outside of circle
 for n in range(1000000):
     if coord() > 1:
         outside += 1
     else:
         inside += 1
 
+# Get the relation between points inside and points in total, then multiply by 4 to get an approximation of pi
 print(inside / (outside + inside) * 4)
-print(at)
